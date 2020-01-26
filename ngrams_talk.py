563824207e061c886edaef06bcb3f203db28ebd7
Olha <olha.v.diakonova@gmail.com>
2020-01-26 22:08:17 +0100

    First commit

---
+++ ngrams_talk.py
@@ -0,0 +1,82 @@
+from nltk import ngrams, word_tokenize, sent_tokenize, ConditionalFreqDist
+import re
+import random
+
+def preprocess_corpus(corpus_file):
+	with open(corpus_file, 'r') as f:
+		corpus = f.read()
+	corpus = sent_tokenize(corpus)
+	corpus = [word_tokenize(s) for s in corpus]
+	return corpus
+
+def make_ngrams(n, corpus):
+	ngrams_ = [tuple(ngrams(s, n, 
+					pad_right=True, right_pad_symbol='</s>',
+					pad_left=True, left_pad_symbol='<s>'))
+				for s in corpus]
+	ngrams_ = [g for t in ngrams_ for g in t]				#flatten the list
+	return ngrams_
+
+
+
+def get_ngram_frequencies(ngrams):
+	freq = ConditionalFreqDist((ngram[:-1], ngram[-1]) for ngram in ngrams)
+	return freq
+
+
+def get_start_sequence(ngrams):
+	while True:
+		start_sequence = random.choice(ngrams)
+		if not re.findall(r"</s>|\.|n't|'s|'m|'d|\,", " ".join(start_sequence)):
+			break
+	return start_sequence
+
+def get_most_common_matches(previous_words, frequencies):
+	previous_words = tuple(previous_words)
+	matches = dict(frequencies[previous_words])
+	matches = sorted(matches, key=matches.get, reverse=True)[:5]
+	return matches
+
+
+def get_next_word(most_common_matches):
+	return random.choice(most_common_matches)
+
+def postprocess_text(text):
+	pass
+
+def generate_text(n, n_sentences, corpus_file):
+	corpus = preprocess_corpus(corpus_file)
+	ngrams_ = make_ngrams(n, corpus)
+	freqs = get_ngram_frequencies(ngrams_)
+	output = []
+	for i in range(n_sentences):
+		output.extend(get_start_sequence(ngrams_))
+		while True:
+			previous_words = output[-(n-1):]
+			matches = get_most_common_matches(previous_words, freqs)
+			next_word = get_next_word(matches)
+			output.append(next_word)
+			if next_word == '</s>':
+				break
+	return output
+
+
+
+	
+n = 3
+print('GENERATED:', generate_text(n, 5, 'witcher_quotes.txt'))
+# grams = make_ngrams(n, preprocess_corpus('witcher_quotes.txt'))
+# print(grams[:50])
+# freqs = get_ngram_frequencies(grams)
+# start = list(freqs[('<s>', '<s>')])
+# print(start)
+# print(random.choice(start))
+
+# print(get_most_common_matches(('<s>', '<s>'), freqs))
+#print(preprocess_corpus('witcher_quotes.txt'))
+# output = []
+#output.extend(get_start_sequence(freqs))
+# print(get_start_sequence(freqs))
+
+# print(output)
+# print(output[-(n-1):])
